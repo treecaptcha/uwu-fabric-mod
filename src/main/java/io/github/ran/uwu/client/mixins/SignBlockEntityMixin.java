c58d6cb15c736340ed3be65429138ab64541d1d7
Ran <43445785+RanCraftPlayz@users.noreply.github.com>
2022-06-25 17:34:37 +0600

    1.19, Closes #4

--- src/main/java/io/github/ran/uwu/client/mixins/SignBlockEntityMixin.java
+++ src/main/java/io/github/ran/uwu/client/mixins/SignBlockEntityMixin.java
@@ -12,7 +12,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(SignBlockEntity.class)
+@Mixin(value = SignBlockEntity.class, priority = -2)
 public abstract class SignBlockEntityMixin {
     @Shadow protected abstract Text[] getTexts(boolean filtered);
 
